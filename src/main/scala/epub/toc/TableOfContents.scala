e02674a8370bd04da0f3ab07fd35cfa173a160d6
Piotr Gabryanczyk <piotrga@gmail.com>
2015-09-02 13:55:15 +0100

    Update TableOfContents.scala

--- src/main/scala/epub/toc/TableOfContents.scala
+++ src/main/scala/epub/toc/TableOfContents.scala
@@ -1,17 +1,1 @@
-package epub.toc
-
-class TableOfContents private (val entries: List[TOCEntry]) {
-  def depth = entries.map(_.depth).max
-}
-
-object TableOfContents {
-
-  def apply(navPoints: TOCEntry*) = {
-    new TableOfContents(navPoints.toList)
-  }
-
-  def entry(label: String, ref: String, children: TOCEntry*) = {
-    new TOCEntry(label, ref, children.toList)
-  }
-}
 
