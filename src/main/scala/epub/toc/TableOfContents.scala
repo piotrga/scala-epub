47c21ef217a19a0fb91cd80630868a0207770503
Piotr Gabryanczyk <piotrga@gmail.com>
2015-09-02 16:53:45 +0100

    Update TableOfContents.scala

--- src/main/scala/epub/toc/TableOfContents.scala
+++ src/main/scala/epub/toc/TableOfContents.scala
@@ -1,1 +1,14 @@
 
+ package epub.toc
+ 
+class TOCEntry private [epub] (val label: String,
+               val ref: String,
+               val children: List[TOCEntry]) {
+
+  def depth: Int = if (children.isEmpty) 1
+  else 1 + children.map(_.depth).max
+}
+
+ class TableOfContents private (val entries: List[TOCEntry]) {
+   def depth = entries.map(_.depth).max
+ }
